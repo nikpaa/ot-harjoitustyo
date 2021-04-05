44fbd29063f348a0d924f97044c02244116d4bed
Niko Paasiniemi <niko.paasiniemi@helsinki.fi>
2021-04-05 10:46:50 +0300

    added support for expressions with multiple operations

--- src/parse.py
+++ src/parse.py
@@ -2,10 +2,19 @@ from operation import Operation
 
 def parse_expression(expression: str) -> float:
     (first_num, rest) = parse_num(expression)
-    (second_num, rest) = parse_num(rest)
-    (operation, rest) = parse_op(rest)
+    return parse_expression_rec(first_num, rest)
 
-    return operation.compute(first_num, second_num)
+
+def parse_expression_rec(first_num: float, expression: str) -> float:
+    if len(expression) == 0:
+        return first_num
+
+    (second_num, rest) = parse_num(expression)
+    (operation, rest) = parse_op(rest)
+    result = operation.compute(first_num, second_num)
+
+    return parse_expression_rec(result, rest)
+
 
 def parse_token(expression: str) -> (str, str):
     current_ind = 0
@@ -16,10 +25,12 @@ def parse_token(expression: str) -> (str, str):
 
     return (current_string, expression[current_ind+1:])
 
+
 def parse_num(expression: str) -> (float, str):
     (num_as_string, rest) = parse_token(expression)
 
     return (float(num_as_string), rest)
+
 
 def parse_op(expression: str) -> (Operation, str):
     (op_as_string, rest) = parse_token(expression)
