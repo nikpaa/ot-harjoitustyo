7cdf29520798a73b7fb514fcd850fde9f7b6343b
Niko Paasiniemi <niko.paasiniemi@helsinki.fi>
2021-04-19 16:18:53 +0300

    simple functionality for previous calculation history added, better still needed

--- src/calculator.py
+++ src/calculator.py
@@ -1,12 +1,24 @@
+import history
 from parse import parse_expression
 
+
 print("Calculator launched.")
+history.create_table()
+history.clear_table()
 
 while True:
     try:
-        laskutoimitus = input("")
-        print(parse_expression(laskutoimitus))
+        operation = input("")
+        result = parse_expression(operation)
+        history.add_operation_to_db(operation, result)
+        print(result)
+        question = input("Do you want to print history (y, n)?")
+        if question == "y":
+            history.print_history()
     except Exception as e:
         print(e)
+        question = input("Do you want to print history (y, n)?")
+        if question == "y":
+            history.print_history()
     
     
