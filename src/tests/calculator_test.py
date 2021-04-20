2ab8beca0ed9715699c39dfdd4f3dc6bca645b90
Niko Paasiniemi <niko.paasiniemi@helsinki.fi>
2021-04-20 17:35:34 +0300

    Added a simple test for parsing

--- src/tests/calculator_test.py
+++ src/tests/calculator_test.py
@@ -1,5 +1,6 @@
 import unittest
 from operation import Operation
+import parse
 
 class TestOperation(unittest.TestCase):
     def setUp(self):
@@ -7,3 +8,7 @@ class TestOperation(unittest.TestCase):
 
     def test_compute(self):      
         self.assertEqual(self.operaatio.compute(1, 1), 2)
+
+class TestParsing(unittest.TestCase):
+    def test_numberparsing(self):
+        self.assertEqual(parse.parse_num("33.5 4 +"), (33.5, "4 +"))
