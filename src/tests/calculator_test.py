0f13fb277d6265b972116190bac28cb2e217a11e
Niko Paasiniemi <niko.paasiniemi@helsinki.fi>
2021-04-26 19:13:48 +0300

    added additional tests for operation

--- src/tests/calculator_test.py
+++ src/tests/calculator_test.py
@@ -1,13 +1,30 @@
 import unittest
 from operation import Operation
 import parse
+from errors import CalculatorError
 
 class TestOperation(unittest.TestCase):
-    def setUp(self):
-        self.operaatio = Operation("+")
+    def test_plus(self):
+        operaatio = Operation("+")
+        self.assertEqual(operaatio.compute(1, 1), 2)
 
-    def test_compute(self):      
-        self.assertEqual(self.operaatio.compute(1, 1), 2)
+    def test_minus(self):
+        operaatio = Operation("-")
+        self.assertEqual(operaatio.compute(2, 1), 1)
+
+    def test_mult(self):
+        operaatio = Operation("*")
+        self.assertEqual(operaatio.compute(40, 5), 200)
+
+    def test_div(self):
+        operaatio = Operation("/")
+        self.assertEqual(operaatio.compute(10, 2), 5)
+
+    def test_div_error(self):
+        operaatio = Operation("/")
+        with self.assertRaises(CalculatorError):
+            operaatio.compute(7, 0)
+    
 
 class TestParsing(unittest.TestCase):
     def test_numberparsing(self):
